f9ab894b3a6f4cb207a1afde3ab207519ac2d26d
Shendidy <sherif@shendidy.com>
2019-05-04 17:22:02 +0100

    can calculate any list total without special offers

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,14 @@
 class Checkout
+  def initialize
+    @list_prices = {"A" => 50, "B" => 30, "C" => 20, "D" => 15}
+    @price = 0
+  end
+
   def checkout(list)
-    list == "AA" ? 100 : -1
+    list.to_s.split('').each{ |item|
+      return -1 unless @list_prices.include? item
+      @price += @list_prices[item]
+    }
+    @price
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,20 +1,23 @@
 require 'checkout'
 
 describe Checkout do
-  shop = Checkout.new
+  before(:each) do
+   @shop = Checkout.new
+  end
+
   it '1- should return -1' do
-    expect(shop.checkout('aBc')).to eq(-1)
+    expect(@shop.checkout('aBc')).to eq(-1)
   end
   it '2- should return -1' do
-    expect(shop.checkout('-B8x')).to eq(-1)
+    expect(@shop.checkout('-B8x')).to eq(-1)
   end
   it '3- should return -1' do
-    expect(shop.checkout(18)).to eq(-1)
+    expect(@shop.checkout(18)).to eq(-1)
   end
   it '4- should return 100' do
-    expect(shop.checkout('AA')).to eq(100)
+    expect(@shop.checkout('AA')).to eq(100)
   end
   it '5- should return 115' do
-    expect(shop.checkout('ABCD')).to eq(115)
+    expect(@shop.checkout('ABCD')).to eq(115)
   end
 end
