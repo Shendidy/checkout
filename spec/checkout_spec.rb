490461b28d5d8b7b1f70390766e5d63fe5ad2059
Shendidy <sherif@shendidy.com>
2019-05-04 15:15:32 +0100

    first test with real stock list

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -5,4 +5,13 @@ describe Checkout do
   it '1- should return -1' do
     expect(shop.checkout('aBc')).to eq(-1)
   end
+  it '2- should return -1' do
+    expect(shop.checkout('-B8x')).to eq(-1)
+  end
+  it '3- should return -1' do
+    expect(shop.checkout(18)).to eq(-1)
+  end
+  it '4- should return 100' do
+    expect(shop.checkout('A')).to eq(100)
+  end
 end
